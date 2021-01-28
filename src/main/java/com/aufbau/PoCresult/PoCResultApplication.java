e3b84e43c48158c258941c7196abf6759a981a25
Kushagra Kalash <kushagrakdev@gmail.com>
2021-01-28 16:09:04 +0530

    initial commit

---
+++ src/main/java/com/aufbau/PoCresult/PoCResultApplication.java
@@ -0,0 +1,13 @@
+package com.aufbau.PoCresult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCResultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aufbau/PoCresult/PoCResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aufbau.PoCresult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

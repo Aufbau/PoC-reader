7c574d933b6e04999a6fb4f0ccd7419cc8be5efd
Kushagra Kalash <kushagrakdev@gmail.com>
2021-01-31 17:49:19 +0530

    Style results page

--- PoC-result/src/main/resources/static/javascript.js
+++ PoC-result/src/main/resources/static/javascript.js
@@ -15,13 +15,25 @@ const main = () => {
 			//clear out previous elements inside result-container
 			rContainer.innerHTML = "";
 
+			let count = 0;
+
+			let styleArr = ["bg-danger", "bg-warning"];
+
 			results.forEach((element) => {
 				//create a div.result for every element in the respose array
 				//and append it inside result-container
 
 				let rdiv = document.createElement("div");
-				rdiv.setAttribute("class", "result");
+				rdiv.setAttribute("class", "result h4 ps-2 py-2");
 				rdiv.innerHTML = `${element.name.toString()}: ${element.count.toString()}`;
+
+				//styling
+				rdiv.className += " " + styleArr[count];
+
+				//for alternate colors
+				count += 1;
+				count = count % 2;
+
 				rContainer.appendChild(rdiv);
 			});
 		})
